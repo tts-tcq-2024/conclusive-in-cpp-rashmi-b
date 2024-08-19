cb2dd78610800c4cff255f36bfdfc26ea49f18c6
Ballamajalu Rashmi <rashmi.rushi@gmail.com>
2024-08-19 11:51:27 +0530

    Added test cases and refactored code

---
+++ notify_user_and_controller.cpp
@@ -0,0 +1,40 @@
+#include "typewise-alert.h"
+#include <stdio.h>
+
+void checkAndAlert(
+    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+
+  BreachType breachType = classifyTemperatureBreach(
+    batteryChar.coolingType, temperatureInC
+  );
+
+  switch(alertTarget) {
+    case TO_CONTROLLER:
+      sendToController(breachType);
+      break;
+    case TO_EMAIL:
+      sendToEmail(breachType);
+      break;
+  }
+}
+
+void sendToController(BreachType breachType) {
+  const unsigned short header = 0xfeed;
+  printf("%x : %x\n", header, breachType);
+}
+
+void sendToEmail(BreachType breachType) {
+  const char* recepient = "a.b@c.com";
+  switch(breachType) {
+    case TOO_LOW:
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too low\n");
+      break;
+    case TOO_HIGH:
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too high\n");
+      break;
+    case NORMAL:
+      break;
+  }
+}
--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,7 +1,72 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
-
-
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(10, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(32, 22, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(22, 20, 30) == NORMAL);
 }
+
+TEST_CASE("classify temperate breach") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 24) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 42) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 48) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 36) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 42) == TOO_HIGH);
+}
+
+TEST_CASE("Test checkAndAlert(to controller)") {
+
+  BatteryCharacter batterych = {
+    .coolingType = HI_ACTIVE_COOLING
+  };
+
+  std::ostringstream toController;
+  std::streambuf* streambuf_1 = std::cout.rdbuf();
+  std::cout.rdbuf(toController.rdbuf());
+
+  checkAndAlert(TO_CONTROLLER, batterych, 50);
+  
+  //restore out stream
+  std::cout.rdbuf(streambuf_1);
+  REQUIRE(toController.str() == "65261 : 2\n");
+}
+
+TEST_CASE("Test checkAndAlert(to email too high)") {
+
+  BatteryCharacter batterych_HAC = {
+    .coolingType = HI_ACTIVE_COOLING
+  };
+
+  std::ostringstream toEmail;
+  std::streambuf* streambuf_1 = std::cout.rdbuf();
+  std::cout.rdbuf(toEmail.rdbuf());
+
+  checkAndAlert(TO_EMAIL, batterych_HAC, 46);
+  
+  //restore out stream
+  std::cout.rdbuf(streambuf_1);
+  REQUIRE(toEmail.str() == "To: user@mail.com\n Alert! temperature is too high\n\n");
+}
+
+TEST_CASE("Test checkAndAlert(to email too low)") {
+  
+  BatteryCharacter batterych_PC = {
+    .coolingType = PASSIVE_COOLING
+  };
+  
+  std::ostringstream toEmail;
+  std::streambuf* streambuf_2 = std::cout.rdbuf();
+  std::cout.rdbuf(toEmail.rdbuf());
+
+  checkAndAlert(TO_EMAIL, batterych_PC, -10);
+  
+  //restore out stream
+  std::cout.rdbuf(streambuf_2);
+  REQUIRE(toEmail.str() == "To: user@mail.com\n Alert! temperature is too low\n");
+}
+
